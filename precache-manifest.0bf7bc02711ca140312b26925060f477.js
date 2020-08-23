072216e1825030713673a25c99b9999a21b90aff
Luan Quach <quachminhluantn@gmail.com>
2020-08-23 15:50:53 +0800

    Updates

---
+++ precache-manifest.0bf7bc02711ca140312b26925060f477.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17ef2ded3d362768397b91e6afa5f199",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7caee7a51d04a79a7532",
+    "url": "/static/css/main.69b80404.chunk.css"
+  },
+  {
+    "revision": "0fee6942e960a84adbc1",
+    "url": "/static/js/2.a6679a79.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/static/js/2.a6679a79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7caee7a51d04a79a7532",
+    "url": "/static/js/main.8705fb51.chunk.js"
+  },
+  {
+    "revision": "b51068bb55fc71968384",
+    "url": "/static/js/runtime-main.23c0c1da.js"
+  },
+  {
+    "revision": "1b631f8e91ff95bc3cd43261d6342672",
+    "url": "/static/media/brain.1b631f8e.jpg"
+  }
+]);
